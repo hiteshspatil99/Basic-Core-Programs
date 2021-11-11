7c98281f750728e94b9f04e174ac445481a9437c
Hitesh <hiteshspatil99@gmail.com>
2021-11-12 00:08:31 +0530

    [Hitesh] add : Power of 2 is Created

---
+++ BasicCoreProblems/BasicCoreProblems/PowerOf2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCoreProblems
+{
+    public class PowerOf2
+    {
+        public void Power()
+        {
+            Console.WriteLine("Enter the power value");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= exponent; i++)
+            {
+                Console.WriteLine("2^{0} " + " " + "= {1}", i, Math.Pow(2, i));
+            }
+        }
+    }
+}
--- BasicCoreProblems/BasicCoreProblems/Program.cs
+++ BasicCoreProblems/BasicCoreProblems/Program.cs
@@ -6,7 +6,7 @@ namespace BasicCoreProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -21,8 +21,12 @@ namespace BasicCoreProblems
                         LeapYear check = new LeapYear();
                         check.Leap();
                         break;
-                        
                     case 3:
+                        PowerOfTwo res = new PowerOfTwo();
+                        res.Power();
+                        break;
+
+                    case 4:
                         flag = false;
                         break;
 
