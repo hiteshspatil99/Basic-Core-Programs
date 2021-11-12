bb5aa137ca51ce6eda47db9522a90240f172abec
Hitesh <hiteshspatil99@gmail.com>
2021-11-13 01:09:36 +0530

    [Hitesh] add : Even Or Odd

---
+++ BasicCoreProblems/BasicCoreProblems/EvenOrOdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCoreProblems
+{
+    public class EvenOrOdd
+    {
+        public void EvenOdd()
+        {
+            Console.WriteLine("Enter the number to Check");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("{0} is an Even number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an Odd number", num);
+            }
+        }
+    }
+}
--- BasicCoreProblems/BasicCoreProblems/Program.cs
+++ BasicCoreProblems/BasicCoreProblems/Program.cs
@@ -6,7 +6,7 @@ namespace BasicCoreProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6.Quotient Remainder \n 7. SwapTwoNumers \n 8. Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6.Quotient Remainder \n 7. SwapTwoNumers \n 8. Even Or Odd Number \n 9. Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -25,12 +25,10 @@ namespace BasicCoreProblems
                         PowerOfTwo res = new PowerOfTwo();
                         res.Power();
                         break;
-
                     case 4:
                         HarmonicNumber sum = new HarmonicNumber();
                         sum.Harmonic();
                         break;
-
                     case 5:
                         Factorial.Factorization();
                         break;
@@ -43,6 +41,10 @@ namespace BasicCoreProblems
                         ans.Swap();
                         break;
                     case 8:
+                        EvenOrOdd num = new EvenOrOdd();
+                        num.EvenOdd();
+                        break;
+                    case 9:
                         flag = false;
                         break;
 
