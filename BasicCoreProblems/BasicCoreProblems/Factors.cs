064aae95ba2589e316e94ade7ef6d72e17c32657
Hitesh <hiteshspatil99@gmail.com>
2021-11-17 23:40:40 +0530

    [Hitesh] add : Calculating The Factors

--- BasicCoreProblems/BasicCoreProblems/Factors.cs
+++ BasicCoreProblems/BasicCoreProblems/Factors.cs
@@ -10,17 +10,12 @@ namespace BasicCoreProblems
         {
             Console.WriteLine("Enter the Number to Calculate Prime Factorization");
             int num = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i * i <= num; i++)
+            for (int i = 2; i <= num; i++)
             {
                 if (num % i == 0)
                 {
-                    int count = 0;
-                    while (num % i == 0)
-                    {
-                        num /= i;
-                        count++;
-                    }
-                    Console.WriteLine($"{i}");
+                    
+                    Console.WriteLine(i);
                 }
             }
         }
